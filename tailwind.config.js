d1e55a20a4822567e0bb23f6f1fafc72f0151c05
Arjuna Jayasinghe <arjuna.j888@gmail.com>
2026-03-12 21:57:43 +0530

    Implement UI for cards & create new task form

---
+++ tailwind.config.js
@@ -0,0 +1,124 @@
+:root {
+            --text-50: #f0f2f4;
+            --text-100: #e1e5ea;
+            --text-200: #c3cbd5;
+            --text-300: #a5b1c0;
+            --text-400: #8897aa;
+            --text-500: #6a7d95;
+            --text-600: #556477;
+            --text-700: #3f4b5a;
+            --text-800: #2a323c;
+            --text-900: #15191e;
+            --text-950: #0b0c0f;
+
+            --background-50: #edf2f7;
+            --background-100: #dbe6f0;
+            --background-200: #b8cce0;
+            --background-300: #94b2d1;
+            --background-400: #7099c2;
+            --background-500: #4d7fb3;
+            --background-600: #3d668f;
+            --background-700: #2e4c6b;
+            --background-800: #1f3347;
+            --background-900: #0f1924;
+            --background-950: #080d12;
+
+            --primary-50: #f0f2f4;
+            --primary-100: #e1e6ea;
+            --primary-200: #c3cdd5;
+            --primary-300: #a5b4c0;
+            --primary-400: #889baa;
+            --primary-500: #6a8295;
+            --primary-600: #556877;
+            --primary-700: #3f4e5a;
+            --primary-800: #2a343c;
+            --primary-900: #151a1e;
+            --primary-950: #0b0d0f;
+
+            --secondary-50: #f1f0f5;
+            --secondary-100: #e4e1ea;
+            --secondary-200: #c9c3d5;
+            --secondary-300: #ada5c0;
+            --secondary-400: #9287ab;
+            --secondary-500: #776996;
+            --secondary-600: #5f5478;
+            --secondary-700: #473f5a;
+            --secondary-800: #302a3c;
+            --secondary-900: #18151e;
+            --secondary-950: #0c0a0f;
+
+            --accent-50: #fff3e6;
+            --accent-100: #fee7cd;
+            --accent-200: #fecf9a;
+            --accent-300: #fdb768;
+            --accent-400: #fda035;
+            --accent-500: #fc8803;
+            --accent-600: #ca6d02;
+            --accent-700: #975102;
+            --accent-800: #653601;
+            --accent-900: #321b01;
+            --accent-950: #190e00;
+
+          }
+          .dark {
+            --text-50: #0b0c0f;
+            --text-100: #15191e;
+            --text-200: #2a323c;
+            --text-300: #3f4b5a;
+            --text-400: #556477;
+            --text-500: #6a7d95;
+            --text-600: #8897aa;
+            --text-700: #a5b1c0;
+            --text-800: #c3cbd5;
+            --text-900: #e1e5ea;
+            --text-950: #f0f2f4;
+
+            --background-50: #080d12;
+            --background-100: #0f1924;
+            --background-200: #1f3347;
+            --background-300: #2e4c6b;
+            --background-400: #3d668f;
+            --background-500: #4d7fb3;
+            --background-600: #7099c2;
+            --background-700: #94b2d1;
+            --background-800: #b8cce0;
+            --background-900: #dbe6f0;
+            --background-950: #edf2f7;
+
+            --primary-50: #0b0d0f;
+            --primary-100: #151a1e;
+            --primary-200: #2a343c;
+            --primary-300: #3f4d5a;
+            --primary-400: #556777;
+            --primary-500: #6a8195;
+            --primary-600: #889aaa;
+            --primary-700: #a5b3c0;
+            --primary-800: #c3cdd5;
+            --primary-900: #e1e6ea;
+            --primary-950: #f0f2f4;
+
+            --secondary-50: #0c0a0f;
+            --secondary-100: #18151e;
+            --secondary-200: #302a3c;
+            --secondary-300: #473f5a;
+            --secondary-400: #5f5478;
+            --secondary-500: #776996;
+            --secondary-600: #9287ab;
+            --secondary-700: #ada5c0;
+            --secondary-800: #c9c3d5;
+            --secondary-900: #e4e1ea;
+            --secondary-950: #f1f0f5;
+
+            --accent-50: #190e00;
+            --accent-100: #321b01;
+            --accent-200: #653601;
+            --accent-300: #975102;
+            --accent-400: #ca6d02;
+            --accent-500: #fc8803;
+            --accent-600: #fda035;
+            --accent-700: #fdb768;
+            --accent-800: #fecf9a;
+            --accent-900: #fee7cd;
+            --accent-950: #fff3e6;
+
+          }
