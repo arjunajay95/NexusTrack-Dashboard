4b77c316cbbacc8d2ddb31ecaed3f525ae1470bd
Arjuna Jayasinghe <arjuna.j888@gmail.com>
2026-03-12 12:14:26 +0530

    Implement darkmode, color themes and Buttons

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // javascript here
-document.getElementById("logo-main").addEventListener("click", () => {
+
+const dark_mode = document.getElementById("dark-mode-toggle");
+const dark_mode_svgs = dark_mode.querySelectorAll("svg");
+const light_mode_icon = dark_mode_svgs[0];
+const dark_mode_icon = dark_mode_svgs[1];
+
+dark_mode.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
+  [light_mode_icon, dark_mode_icon].forEach((svg) => {
+    svg.classList.toggle("hidden");
+  });
 });
