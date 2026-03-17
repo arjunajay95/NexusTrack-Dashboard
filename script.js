8c6bcc14781ac611864c55c9d56c142bc7c65d19
Arjuna Jayasinghe <arjuna.j888@gmail.com>
2026-03-17 21:43:05 +0530

    Implement FAQ toggles and debug issues

--- script.js
+++ script.js
@@ -298,7 +298,7 @@ function task_card(tasks_list, task_container) {
                       </div>
                     </div>
                   </div>
-                  <div id="delete-${task.task_id}" class="delete-task text-text-500 hover:text-rose-500 hover:bg-rose-500/10 opacity-0 rounded-lg p-1 cursor-pointer transition-opacity duration-200">
+                  <div id="delete-${task.task_id}" class="delete-task text-text-500 lg:hover:text-rose-500 lg:hover:bg-rose-500/10 active:text-rose-500 active:bg-rose-500/10 lg:opacity-0 rounded-lg p-1 cursor-pointer transition-opacity duration-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                       <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
@@ -326,10 +326,10 @@ function task_card_features() {
 
     // Delete btn toggle over mouse pointer hover
     card.addEventListener("mouseover", () => {
-      delete_task.classList.replace("opacity-0", "opacity-100");
+      delete_task.classList.replace("lg:opacity-0", "lg:opacity-100");
     });
     card.addEventListener("mouseout", () => {
-      delete_task.classList.replace("opacity-100", "opacity-0");
+      delete_task.classList.replace("lg:opacity-100", "lg:opacity-0");
     });
 
     // Change status with btn click event
@@ -629,6 +629,7 @@ taskForm.addEventListener("submit", (e) => {
   const new_task_status = document.getElementById("new-task-status").value;
   const new_task_date = document.getElementById("new-task-date").value;
 
+  // Add new task dictionary entry
   const new_task = {
     task_id: tasks_list.length + 1,
     task_name: new_task_name,
@@ -638,11 +639,12 @@ taskForm.addEventListener("submit", (e) => {
     date: new_task_date,
   };
 
+  // Append the new task to the list
+  // Update task containers
   tasks_list.push(new_task);
   tab_lists_update();
   filter_tasks();
   search_tasks();
-  console.log(tasks_list);
 
   // Show the success message (this triggers the downward expansion)
   success_msg.classList.remove("max-h-0", "opacity-0");
@@ -652,4 +654,29 @@ taskForm.addEventListener("submit", (e) => {
   setTimeout(() => {
     close_form();
   }, 2000);
+});
+
+// ---------------------------------------FAQ toggles
+
+const faq_blocks = document.querySelectorAll(".faq-block");
+
+faq_blocks.forEach((block) => {
+  const answer = block.querySelector("p");
+
+  block.addEventListener("click", () => {
+    const isHidden = answer.classList.contains("opacity-0");
+    const icon = block.querySelector(".accordion-chevron");
+
+    if (isHidden) {
+      answer.classList.replace("max-h-0", "max-h-20");
+      answer.classList.replace("opacity-0", "opacity-100");
+      answer.classList.add("mx-3", "mb-3");
+      icon.style.transform = "rotate(0deg)";
+    } else {
+      answer.classList.replace("max-h-20", "max-h-0");
+      answer.classList.replace("opacity-100", "opacity-0");
+      answer.classList.remove("mx-3", "mb-3");
+      icon.style.transform = "rotate(180deg)";
+    }
+  });
 });
