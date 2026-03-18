54e4a2c2a394e3c60d09c834172b43630df82aa9
Arjuna Jayasinghe <arjuna.j888@gmail.com>
2026-03-18 09:30:30 +0530

    Implement empty tasks container & mobile menu close function

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ const dark_mode_icon = dark_mode_svgs[1];
 // task element selections
 const task_container = document.getElementById("task-container");
 const task_tabs = document.querySelectorAll(".task-tab");
+const task_empty = document.getElementById("empty-tasks");
 
 // sort element selections
 const btn_sort = document.getElementById("btn-sort");
@@ -238,80 +239,86 @@ function set_status_completed() {
 // task card generate function
 function task_card(tasks_list, task_container) {
   task_container.innerHTML = "";
-  for (const task of tasks_list) {
-    // check task status
-    let status_complete_text_color = "text-text-900";
-    let status_complete_text_deco = "";
 
-    if (task.task_status.toLowerCase() === "to-do") {
-      set_status_todo();
-    } else if (task.task_status.toLowerCase() === "in-progress") {
-      set_status_inprogress();
-    } else {
-      set_status_completed();
-      status_complete_text_color = "text-text-500";
-      status_complete_text_deco = "line-through";
-    }
+  if (tasks_list.length === 0) {
+    task_empty.classList.remove("hidden");
+  } else {
+    task_empty.classList.add("hidden");
+    for (const task of tasks_list) {
+      // check task status
+      let status_complete_text_color = "text-text-900";
+      let status_complete_text_deco = "";
 
-    // check task priority
-    let priority_text_color,
-      priority_bg_color,
-      priority_ring_color,
-      priority_text = "";
+      if (task.task_status.toLowerCase() === "to-do") {
+        set_status_todo();
+      } else if (task.task_status.toLowerCase() === "in-progress") {
+        set_status_inprogress();
+      } else {
+        set_status_completed();
+        status_complete_text_color = "text-text-500";
+        status_complete_text_deco = "line-through";
+      }
 
-    if (task.task_priority.toLowerCase() === "low") {
-      priority_text_color = "text-green-500";
-      priority_bg_color = "bg-green-500/10";
-      priority_ring_color = "ring-green-200/80";
-      priority_text = "Low";
-    } else if (task.task_priority.toLowerCase() === "medium") {
-      priority_text_color = "text-amber-500";
-      priority_bg_color = "bg-amber-500/10";
-      priority_ring_color = "ring-amber-200/80";
-      priority_text = "Medium";
-    } else {
-      priority_text_color = "text-rose-500";
-      priority_bg_color = "bg-rose-500/10";
-      priority_ring_color = "ring-rose-200/80";
-      priority_text = "High";
-    }
+      // check task priority
+      let priority_text_color,
+        priority_bg_color,
+        priority_ring_color,
+        priority_text = "";
 
-    //format date
-    const date_obj = new Date(task.date);
-    const formatted_date = date_obj.toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric" });
+      if (task.task_priority.toLowerCase() === "low") {
+        priority_text_color = "text-green-500";
+        priority_bg_color = "bg-green-500/10";
+        priority_ring_color = "ring-green-200/80";
+        priority_text = "Low";
+      } else if (task.task_priority.toLowerCase() === "medium") {
+        priority_text_color = "text-amber-500";
+        priority_bg_color = "bg-amber-500/10";
+        priority_ring_color = "ring-amber-200/80";
+        priority_text = "Medium";
+      } else {
+        priority_text_color = "text-rose-500";
+        priority_bg_color = "bg-rose-500/10";
+        priority_ring_color = "ring-rose-200/80";
+        priority_text = "High";
+      }
 
-    task_container.innerHTML += `
-              <!-- Task card -->
-              <article id="task-${task.task_id}" class="task-card flex flex-col justify-center items-start border border-background-200/50 rounded-xl p-4 hover:shadow-md transition-all duration-200">
-                <div class="flex w-full justify-between items-start">
-                  <div class="flex flex-col items-start gap-2">
-                    <p id="task-name" class="task-name ${status_complete_text_color} ${status_complete_text_deco} text-sm font-semibold">${task.task_name}</p>
-                    <p id="task-description" class="text-text-500 text-xs">${task.task_desc}</p>
-                    <div class="flex gap-2 justify-start items-center text-xs mt-1">
-                      <!-- Priority badge -->
-                      <div class="">
-                        <p class="text-[11px] font-medium tracking-wide ${priority_text_color} ${priority_bg_color} ring-1 ${priority_ring_color} py-0.5 px-2 rounded-full">${priority_text}</p>
-                      </div>
-                      <!-- Progress badge -->
-                      <div class="progress-badge">
-                        <p class="text-[11px] font-medium ${status_text_color} ${status_bg_color} py-0.5 px-2 rounded-full tracking-wide">${status_text}</p>
+      //format date
+      const date_obj = new Date(task.date);
+      const formatted_date = date_obj.toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric" });
+
+      task_container.innerHTML += `
+                <!-- Task card -->
+                <article id="task-${task.task_id}" class="task-card flex flex-col justify-center items-start border border-background-200/50 rounded-xl p-4 hover:shadow-md transition-all duration-200">
+                  <div class="flex w-full justify-between items-start">
+                    <div class="flex flex-col items-start gap-2">
+                      <p id="task-name" class="task-name ${status_complete_text_color} ${status_complete_text_deco} text-sm font-semibold">${task.task_name}</p>
+                      <p id="task-description" class="text-text-500 text-xs">${task.task_desc}</p>
+                      <div class="flex gap-2 justify-start items-center text-xs mt-1">
+                        <!-- Priority badge -->
+                        <div class="">
+                          <p class="text-[11px] font-medium tracking-wide ${priority_text_color} ${priority_bg_color} ring-1 ${priority_ring_color} py-0.5 px-2 rounded-full">${priority_text}</p>
+                        </div>
+                        <!-- Progress badge -->
+                        <div class="progress-badge">
+                          <p class="text-[11px] font-medium ${status_text_color} ${status_bg_color} py-0.5 px-2 rounded-full tracking-wide">${status_text}</p>
+                        </div>
                       </div>
                     </div>
+                    <div id="delete-${task.task_id}" class="delete-task text-text-500 lg:hover:text-rose-500 lg:hover:bg-rose-500/10 active:text-rose-500 active:bg-rose-500/10 lg:opacity-0 rounded-lg p-1 cursor-pointer transition-opacity duration-200">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      </svg>
+                    </div>
                   </div>
-                  <div id="delete-${task.task_id}" class="delete-task text-text-500 lg:hover:text-rose-500 lg:hover:bg-rose-500/10 active:text-rose-500 active:bg-rose-500/10 lg:opacity-0 rounded-lg p-1 cursor-pointer transition-opacity duration-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
+                  <div class="flex justify-between items-center border-t border-background-200/50 w-full pt-4 mt-4">
+                    <p id="date" class="text-text-500 text-[11px]">${formatted_date}</p>
+                    <button id="btn-${task.task_id}"><span class="text-text-900 text-[11px] font-medium border rounded-lg border-background-200/50 px-2 py-1 hover:bg-background-100/50 dark:hover:bg-background-200 cursor-pointer">${status_btn}</span></button>
                   </div>
-                </div>
-                <div class="flex justify-between items-center border-t border-background-200/50 w-full pt-4 mt-4">
-                  <p id="date" class="text-text-500 text-[11px]">${formatted_date}</p>
-                  <button id="btn-${task.task_id}"><span class="text-text-900 text-[11px] font-medium border rounded-lg border-background-200/50 px-2 py-1 hover:bg-background-100/50 dark:hover:bg-background-200 cursor-pointer">${status_btn}</span></button>
-                </div>
-              </article>
-    `;
+                </article>
+      `;
+    }
+    task_card_features();
   }
-  task_card_features();
 }
 
 // task card btn features function
@@ -373,6 +380,11 @@ function task_card_features() {
       tasks_list = tasks_list.filter((task) => task.task_id != task_Id);
       tab_lists_update();
       card.remove();
+
+      // If no tasks left, visible empty tasks message
+      if (tasks_list.length === 0) {
+        task_empty.classList.remove("hidden");
+      }
     });
   });
 }
@@ -409,6 +421,17 @@ function filter_tasks() {
       }
     });
   });
+}
+
+// Close mobile menu function
+function mobile_menu_close() {
+  overlay.classList.remove("opacity-100");
+  overlay.classList.add("opacity-0");
+  overlay.classList.remove("pointer-events-auto");
+  overlay.classList.add("pointer-events-none");
+  mobile_menu.classList.add("hidden");
+  hamburger_toggle.children[0].classList.remove("hidden");
+  hamburger_toggle.children[1].classList.add("hidden");
 }
 
 // Search function
@@ -526,6 +549,7 @@ dark_mode.addEventListener("click", () => {
 
 // ------------------------------------------------------------ Mobile Menu toggle
 
+// Open / close mobile menu with hamburger icon click
 hamburger_toggle.addEventListener("click", () => {
   Array.from(hamburger_toggle.children).forEach((child) => {
     child.classList.toggle("hidden");
@@ -539,6 +563,9 @@ hamburger_toggle.addEventListener("click", () => {
   }
   mobile_menu.classList.toggle("hidden");
 });
+
+// Close mobile menu when clicking outside overlay
+overlay.addEventListener("click", mobile_menu_close);
 
 // ------------------------------------------------------------ User Profile Sidebar Menu toggle
 
